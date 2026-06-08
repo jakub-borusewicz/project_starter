cefeb5f956969f7e0aa9c70da900f0e610877447
jakubborusewicz <jakub.borusewicz@gmail.com>
2026-06-08 10:13:29 +0200

    wip

--- tests/test_generation.py
+++ tests/test_generation.py
@@ -1,6 +1,5 @@
 import pathlib
 import copier
-from copier import settings
 
 from tests.consts import BASE_DIR
 
@@ -10,18 +9,16 @@ class TestProjectGeneration:
 
     def test_generation_succeed(self, tmp_path: pathlib.Path) -> None:
         # given
+        generated_project_dir = tmp_path / "test_project"
 
         # when
         copier.run_copy(
             src_path=str(BASE_DIR),
-            dst_path=tmp_path,
+            dst_path=generated_project_dir,
             data={
                 "project_name": "test_project",
             },
             unsafe=True,
-            # settings=settings.Settings(
-            #     trust=True
-            # )
         )
 
         # then
